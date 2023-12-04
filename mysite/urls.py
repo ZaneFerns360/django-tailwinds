dec55bd9eb3203f404477ce14252606b30e20e36
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-04 23:36:01 +0530

    Auth added

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, LoginView, signup
+from .views import CustomLogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("signup/", signup, name="signup"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
 ]
