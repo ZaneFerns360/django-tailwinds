6010659718647394f7c59132cf635ac9035b1bc7
ZaneFerns360 <zane.ferns360@gmail.com>
2023-12-05 00:09:22 +0530

    added subprocess

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ npm install
 # or
 pnpm install
 ```
-## Start Tailwinds static output:
+## Start Tailwinds static output (Automatic):
+- check the node js pakage manager in manage.py (default = pnpm), if your are using npm then replace 'pnpm' with 'npm'
+- proceed to run server as normal
+## Start Tailwinds static output (Manual):
 ```bash
 npx tailwindcss -i ./static/src/input.css -o ./static/src/output.css --watch
 # or
--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +16,20 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if sys.argv[1] == "runserver":
+        subprocess.Popen(
+            [
+                "pnpm",
+                "tailwindcss",
+                "-i",
+                "./static/src/input.css",
+                "-o",
+                "./static/src/output.css",
+                "--watch",
+            ]
+        )
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
